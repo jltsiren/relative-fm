fc0042e2330e149ee5ff6de0ebde6225264abe6e
Jouni Sirén <jouni.siren@iki.fi>
2014-05-02 23:26:54 +0300

    Changed default WT bitvector to RRR.

--- utils.h
+++ utils.h
@@ -78,7 +78,8 @@ uint64_t readRows(const std::string& filename, std::vector<std::string>& rows, b
 
 //------------------------------------------------------------------------------
 
-typedef wt_huff<bit_vector, rank_support_v5<> > bwt_type;
+//typedef wt_huff<bit_vector, rank_support_v5<> > bwt_type;
+typedef wt_huff<rrr_vector<63> > bwt_type;
 typedef byte_alphabet alphabet_type;
 
 inline uint64_t
